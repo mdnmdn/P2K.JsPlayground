83bebac5e9ff5b6f054465a8289f03c55af5fa84
mdnmdn <marco.denittis@gmail.com>
2016-07-05 12:47:40 +0200

    guess a number + badge

--- P2K.WebApp/P2K.WebApp/Controllers/GuessNumberController.cs
+++ P2K.WebApp/P2K.WebApp/Controllers/GuessNumberController.cs
@@ -8,11 +8,61 @@ namespace P2K.WebApp.Controllers
 {
     public class GuessNumberController : Controller
     {
-        //static int? 
+        static int? _number;
+        static int _trials;
 
-        public ActionResult Index()
-        {
-            return View();
+        public GuessNumberController() {
+            if (!_number.HasValue) GenerateNumber();
         }
+
+        private void GenerateNumber()
+        {
+            _trials = 0;
+            _number = new Random().Next(100);
+        }
+
+        [HttpPost]
+        public ActionResult Reset()
+        {
+            GenerateNumber();
+            return Json(new { 
+                trials = _trials,
+                status = "ok" 
+            });
+        }
+
+        [HttpPost]
+        public ActionResult Guess(int number)
+        {
+            if (number == _number) {
+                var result = new { 
+                    trials = _trials,
+                    status = "ok",
+                    number = _number 
+                };
+
+                GenerateNumber();
+
+                return Json(result);
+            }
+
+
+            _trials++;
+            var direction = _number > number ? "greater" : "smaller";
+
+            return Json(new
+            {
+                trials = _trials,
+                status = direction,
+                message = "The misterious number is " + direction + " than " + number
+            });
+        }
+
+        public ActionResult Trials()
+        {
+            return Json(new { trials= _trials },JsonRequestBehavior.AllowGet);
+        }
+
+
     }
 }
